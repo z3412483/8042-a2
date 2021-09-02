da117bcd7ce3b8f25b220091efae5a6eb7662374
z3412483 <z3412483@ad.unsw.edu.au>
2021-09-02 20:10:52 +1000

    start GTK development to display window. Moving to windows development environment to build the GKT app more easily (fingers crossed)

--- app/count_letter_from_file/count.c
+++ app/count_letter_from_file/count.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <gtk/gtk.h>
+
 void drop_newline(char *in_string)
 {
     in_string[strcspn(in_string, "\n")] = 0;
@@ -36,8 +38,42 @@ void str_count()
     printf("Found String: '%s' %d times in file %s.\n", str, count, file_name);
 }
 
-int main()
-{
-    str_count();
-    return 0;
+// callback function which is called when button is clicked
+static void on_button_clicked(GtkButton *btn, gpointer data) {
+    // change button label when it's clicked
+    gtk_button_set_label(btn, "Hello World");
+}
+
+// callback function which is called when application is first started
+static void on_app_activate(GApplication *app, gpointer data) {
+    // create a new application window for the application
+    // GtkApplication is sub-class of GApplication
+    // downcast GApplication* to GtkApplication* with GTK_APPLICATION() macro
+    GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(app));
+    // a simple push button
+    GtkWidget *btn = gtk_button_new_with_label("Click Me!");
+    // connect the event-handler for "clicked" signal of button
+    g_signal_connect(btn, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    // add the button to the window
+    gtk_container_add(GTK_CONTAINER(window), btn);
+    // display the window
+    gtk_widget_show_all(GTK_WIDGET(window));
+}
+
+int main(int argc, char *argv[]) {
+    // create new GtkApplication with an unique application ID
+    GtkApplication *app = gtk_application_new(
+        "org.gtkmm.example.HelloApp", 
+        G_APPLICATION_FLAGS_NONE
+    );
+    // connect the event-handler for "activate" signal of GApplication
+    // G_CALLBACK() macro is used to cast the callback function pointer
+    // to generic void pointer
+    g_signal_connect(app, "activate", G_CALLBACK(on_app_activate), NULL);
+    // start the application, terminate by closing the window
+    // GtkApplication* is upcast to GApplication* with G_APPLICATION() macro
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    // deallocate the application object
+    g_object_unref(app);
+    return status;
 }
